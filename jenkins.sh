c673356173d299e08d24888a9b1c87986dc04b77
Benjamin Borbe <bborbe@rocketnews.de>
2015-11-20 23:08:47 +0100

    add jenkins build

---
+++ jenkins.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+SOURCEDIRECTORY="github.com/bborbe/debian-utils"
+INSTALLS="github.com/bborbe/debian-utils/bin/create_debian_package github.com/bborbe/debian-utils/bin/create_debian_package_by_config"
+VERSION="1.0.1-b${BUILD_NUMBER}"
+NAME="debian-utils"
+
+export GOROOT=/opt/go1.5.1
+export PATH=$GOROOT/bin:$PATH
+export GOPATH=${WORKSPACE}
+export REPORT_DIR=${WORKSPACE}/test-reports
+DEB="${NAME}_${VERSION}.deb"
+rm -rf $REPORT_DIR ${WORKSPACE}/*.deb
+mkdir -p $REPORT_DIR
+PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | /usr/lib/go/bin/dirof | /usr/lib/go/bin/unique`
+FAILED=false
+for PACKAGE in $PACKAGES
+do
+    XML=$REPORT_DIR/`/usr/lib/go/bin/pkg2xmlname $PACKAGE`
+    OUT=$XML.out
+    go test -i $PACKAGE
+    go test -v $PACKAGE | tee $OUT
+    cat $OUT
+	/usr/lib/go/bin/go2xunit -fail=true -input $OUT -output $XML
+	rc=$?
+	if [ $rc != 0 ]
+	then
+	    echo "Tests failed for package $PACKAGE"
+    	FAILED=true
+	fi
+done
+
+if $FAILED
+then
+  echo "Tests failed => skip install"
+  exit 1
+else
+  echo "Tests success"
+fi
+
+echo "Tests completed, install to $GOPATH"
+
+go install $INSTALLS
+
+echo "Install completed, create debian package"
+
+/opt/debian/bin/create_debian_package_by_config \
+-loglevel=DEBUG \
+-version=$VERSION \
+-config=src/$SOURCEDIRECTORY/create_debian_package_config.json
+
+echo "Create debian package completed, upload"
+
+/opt/aptly/bin/aptly_upload \
+-loglevel=DEBUG \
+-url=http://aptly.benjamin-borbe.de \
+-username=api \
+-password=KYkobxZ6uvaGnYBG \
+-file=$DEB \
+-repo=unstable
+
+echo "Upload completed"
