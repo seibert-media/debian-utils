eede226af531ab9a90f60790aff2ea28c9a67580
Benjamin Borbe <bborbe@rocketnews.de>
2015-12-17 16:04:27 +0100

    combined creator

--- jenkins.sh
+++ jenkins.sh
@@ -16,18 +16,18 @@ PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | /opt/utils/bin/dir
 FAILED=false
 for PACKAGE in $PACKAGES
 do
-    XML=$REPORT_DIR/`/opt/utils/bin/pkg2xmlname $PACKAGE`
-    OUT=$XML.out
-    go test -i $PACKAGE
-    go test -v $PACKAGE | tee $OUT
-    cat $OUT
-	/opt/go2xunit/bin/go2xunit -fail=true -input $OUT -output $XML
-	rc=$?
-	if [ $rc != 0 ]
-	then
-	    echo "Tests failed for package $PACKAGE"
-    	FAILED=true
-	fi
+  XML=$REPORT_DIR/`/opt/utils/bin/pkg2xmlname $PACKAGE`
+  OUT=$XML.out
+  go test -i $PACKAGE
+  go test -v $PACKAGE | tee $OUT
+  cat $OUT
+  /opt/go2xunit/bin/go2xunit -fail=true -input $OUT -output $XML
+  rc=$?
+  if [ $rc != 0 ]
+  then
+    echo "Tests failed for package $PACKAGE"
+    FAILED=true
+  fi
 done
 
 if $FAILED
@@ -44,7 +44,7 @@ go install $INSTALLS
 
 echo "Install completed, create debian package"
 
-/opt/debian/bin/create_debian_package_by_config \
+/opt/debian/bin/create_debian_package \
 -loglevel=DEBUG \
 -version=$VERSION \
 -config=src/$SOURCEDIRECTORY/create_debian_package_config.json || exit 1
