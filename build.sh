08c4ab1e7d770d57b9a7e657212b72e6bfcec11c
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-29 10:09:25 +0200

    log workspace

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ NAME="debian-utils"
 
 ################################################################################
 
+echo "use workspace ${WORKSPACE}"
+
 export GOROOT=/opt/go
 export PATH=/opt/go2xunit/bin/:/opt/utils/bin/:/opt/aptly_utils/bin/:/opt/aptly/bin/:/opt/debian_utils/bin/:/opt/debian/bin/:$GOROOT/bin:$PATH
 export GOPATH=${WORKSPACE}
@@ -40,7 +42,7 @@ else
   echo "Tests success"
 fi
 
-echo "Tests completed, install to $GOPATH"
+echo "Tests completed, install to ${GOPATH}"
 
 go install $INSTALLS
 
